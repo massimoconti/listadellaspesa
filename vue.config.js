01c7daac560746517531880a6158241ac95dace0
strang82 <37350933+strang82@users.noreply.github.com>
2019-04-22 19:09:58 +0200

    Fix prox public url

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/listaspesa/'
+    : '/',
   // ...other vue-cli plugin options...
   pwa: {
     name: 'Lista della Spesa',
