a61bf3bae0c9b2b386e036298089d6902dcf1139
strang82 <37350933+strang82@users.noreply.github.com>
2019-04-23 08:59:32 +0200

    firebase deploy
    android assetlinks twa

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/listaspesa/'
-    : '/',
   // ...other vue-cli plugin options...
   pwa: {
     name: 'Lista della Spesa',
