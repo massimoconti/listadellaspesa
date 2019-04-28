4dc94295bc99c1bf3338084a32e3fbb655bf8129
strang82 <37350933+strang82@users.noreply.github.com>
2019-04-28 22:04:32 +0200

    fixup! Fix color tips in List.vue

---
+++ src/components/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="grey--text text--darken-2">
+    <p class="display-1">:-/</p>
+    <p class="">La pagina richiesta non esiste</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'not-found',
+}
+</script>
