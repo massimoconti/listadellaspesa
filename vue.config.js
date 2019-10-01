0d8343c04c2a45dbc144a2a15925a264577a4a89
strang82 <37350933+strang82@users.noreply.github.com>
2019-10-01 22:51:46 +0200

    Service worker modalità InjectManifest per fix caching font

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  // ...other vue-cli plugin options...
   pwa: {
     name: 'Lista della Spesa',
     themeColor: '#01a3e1',
     msTileColor: '#0C7DAF',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       importWorkboxFrom: 'local',
-      offlineGoogleAnalytics: true,
-      skipWaiting: true,
+      swSrc: './src/service-worker.js',
     }
   }
 }
